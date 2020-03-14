84d57563ca8aa0de9b910398d138f0cddd4e9253
d01masatooo11 <d01masatooo11@gmail.com>
2020-03-14 10:59:23 +0900

    add:各userの作成
    管理者、一般ユーザ、専門家で作成sita

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_for :experts, controllers: {
+    sessions:      'experts/sessions',
+    passwords:     'experts/passwords',
+    registrations: 'experts/registrations'
+  }
   root 'top#home'
 end
