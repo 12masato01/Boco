1ad20bdc33f233bcff0fa06fc0c17fecd78891c1
d01masatooo11 <d01masatooo11@gmail.com>
2020-03-16 19:09:34 +0900

    fix:不要な文字・スペースの削除

--- db/schema.rb
+++ db/schema.rb
@@ -36,17 +36,6 @@ ActiveRecord::Schema.define(version: 2020_03_14_065932) do
     t.index ["reset_password_token"], name: "index_experts_on_reset_password_token", unique: true
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+# Could not dump table "users" because of following ActiveRecord::StatementInvalid
+#   Mysql2::Error::ConnectionError: Lost connection to MySQL server during query: SHOW CREATE TABLE `users`
 
-end
