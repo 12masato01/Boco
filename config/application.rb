4972eccdee4790a25876436c3037b7089e57675c
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-04 12:01:25 +0900

    add:アプリとdeviseの日本語化に必要な設定・ファイルの追加
    一旦テンプレートを導入。ここから修正していく。

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module Boco
                        helper_specs: false,
                        outing_specs: false
     end
+
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
   end
 end
