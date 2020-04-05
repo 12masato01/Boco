eb2fe6d3f598d83bf9583683e0427c72706a2238
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-05 18:57:17 +0900

    add:ユーザー・質問モデル周囲のテストを作成

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,4 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  let!(:question) { create(:question) }
+
+  describe "バリデーション" do
+    context "titleとcontentが入力されている場合" do
+      it "登録に成功する" do
+        expect(question).to be_valid
+      end
+    end
+
+    context "titleが空の場合" do
+      it "投稿に失敗する" do
+        question = build(:question, title: nil)
+        question.valid?
+      end
+    end
+
+    context "contentが空の場合" do
+      it "投稿に失敗する" do
+        question = build(:question, content: nil)
+        question.valid?
+      end
+    end
+
+    context "user_idが空の場合" do
+      it "投稿に失敗する" do
+        question = build(:question, user_id: nil)
+        question.valid?
+      end
+    end
+  end
 end
