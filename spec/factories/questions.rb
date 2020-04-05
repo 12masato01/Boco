4fc4df11232f03323e9ef6521a0505b9f42e68cf
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-05 17:38:13 +0900

    質問モデルのテストを記載

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :question do
-    content { "MyText" }
-    user { nil }
+    sequence(:title) { |n| "test_name#{n}"}
+    sequence(:email) { |n| "test_email#{n}@example.com"}
+    password { "password" }
+    password_confirmation { "password" }
+  end
   end
 end
