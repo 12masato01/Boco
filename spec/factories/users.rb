384bebf921b9771280b11ca62ac71b566d28e227
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-07 16:49:06 +0900

    fix:インデント調整

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test_name#{n}" }
-    sequence(:email) { |n| "example#{n}@example.com" }
+    sequence(:email) { |n| "test#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
