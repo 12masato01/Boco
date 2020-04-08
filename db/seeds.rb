117c49fbbe9b6e14291792dd444c7aac15e4fff1
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-08 18:22:24 +0900

    add:専門家ユーザーの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-User.find_or_create_by(id: 1) do |user|
-  user.email: "d01masatooo11@gmail.com",
-  user.password: "masato1201",
-)
+Admin.find_or_create_by(id: 1) do |admin|
+  admin.email = "d01masatooo11@gmail.com"
+  admin.password = "masato1201"
+end
+
+Expert.find_or_create_by(id: 1) do |expert|
+  expert.email = "d01masatooo11@gmail.com"
+  expert.password = "masato1201"
+end
