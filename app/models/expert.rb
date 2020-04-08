277aab993c61049b98925b9aeb8cf0e13431a8c9
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-08 18:47:41 +0900

    add:answerモデルの作成

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,4 +1,9 @@
 class Expert < ApplicationRecord
+  has_many :answers, dependent: :destroy
+  belongs_to :question, dependent: :destroy
+  validates :expert_id, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, #:registerable,
