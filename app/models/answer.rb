dacfa7addf24b3e68bd4791737604d7ddf37bbd8
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-08 18:49:17 +0900

    fix:バリテーション記載場所を修正

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :expert
   belongs_to :question
+  validates :expert_id, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
