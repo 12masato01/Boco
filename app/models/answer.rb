ce86482aa7e607ab8ae81d23cc2cf973bcd71455
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-09 12:28:02 +0900

    delete:回答のタイトルを削除

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :expert
   belongs_to :question
   validates :expert_id, presence: true
-  validates :title, presence: true
   validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_094413) do
+ActiveRecord::Schema.define(version: 2020_04_09_032636) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2020_04_08_094413) do
   end
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.bigint "expert_id"
     t.bigint "question_id"
