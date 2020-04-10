2cac85cba5ab218e4e2adfa3aac541f503097e33
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-10 22:09:52 +0900

    fix:関連付けを修正

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ApplicationRecord  
   belongs_to :user
   belongs_to :question
+  validates :question_id, presence: true
   validates :user_id, presence: true
   validates :content, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
-  has_many :answer, dependent: :destroy
+  has_many :answers, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :title, presence: true
