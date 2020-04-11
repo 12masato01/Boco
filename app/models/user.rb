578e40882d60689c4d93e0fa9eb3b092c5fc6f81
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-11 15:19:40 +0900

    fix:コメントに、user_idを外部キーとして設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
-  has_many :comments, as: :commentable
+  has_many :comments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
