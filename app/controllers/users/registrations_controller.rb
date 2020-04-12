de3ac4a153839b91f4d3ee9e4bd20558922f945a
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-13 00:47:40 +0900

    add:ゲストユーザーを編集・削除できないようにした。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+    before_action :check_guest, only: %i(update destroy)
 
   # GET /resource/sign_up
   # def new
@@ -37,6 +38,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
+  def check_guest
+    if resource.email == "guest@example.com"
+      redirect_to root_path, danger: "ゲストユーザーは編集・削除できません。"
+    end
+  end
 
   protected
 
