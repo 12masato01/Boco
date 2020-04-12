db134b5b4c8090e8767507c4fa202da102cd9f3c
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-12 17:45:42 +0900

    fix:comment_spec完成

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Comments", type: :system do
 
   describe "質問詳細ページ" do
     before do
+      create(:comment, :for_question, content: "こんな解決策があります。", user: user)
       sign_in user
       visit question_path(question)
     end
@@ -16,26 +17,35 @@ RSpec.describe "Comments", type: :system do
       it "投稿に成功し、フラッシュメッセージを表示する" do
         fill_in "comment[content]", with: "僕も気になります。"
         click_button "コメントする"
-        expect(page).to have_content "質問が保存されました"
-        expect(current_path).to eq questions_path(question)
+        expect(page).to have_content "コメントが投稿されました"
+        expect(current_path).to eq question_path(question)
         expect(page).to have_content "僕も気になります。"
       end
     end
 
-    #フォームの入力が誤りであるテストを記載
+    context "フォームの入力が無い時" do
+      it "投稿に失敗し、フラッシュメッセージを表示する" do
+        fill_in "comment[content]", with: ""
+        click_button "コメントする"
+        expect(page).to have_content "コメントの投稿に失敗しました"
+      end
+    end
 
     context "ログインユーザーとコメント投稿者が一致する時" do
       it "削除のリンクを表示する" do
-       expect(page).to have_link "削除"
+        expect(page).to have_link "削除"
       end
+    end
 
       it "コメントを削除をする" do
-        click_link "削除"
-        expect(current_path).to question_path(question)
+        within "timestamp" do
+          click_link "削除"
+        end
+        expect(current_path).to eq question_path(question)
         expect(page).to have_content "コメントが削除されました"
       end
 
-      context "ログインユーザーと、質問投稿者が一致しない時" do
+    context "ログインユーザーと、質問投稿者が一致しない時" do
       before do
       sign_in other_user
       visit question_path(question)
