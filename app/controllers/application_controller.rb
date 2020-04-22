9342dc6a209249a987991a6346008a4f332bc3f3
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-22 15:43:17 +0900

    add:検索機能の実装
    タイトル検索できるようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_serch
   helper_method :user_info
 
   def user_info(user_id)
     User.find(user_id)
+  end
+
+  def set_serch
+    @q = Question.ransack(params[:q])
+    @search_questions = @q.result(distinct: true)
   end
 
   protected
