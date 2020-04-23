74cfd3ff22f5a9354b1e15c833e14e0d91d452c5
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-23 12:41:39 +0900

    add:画像・動画サイズのバリテーション追加

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,5 +5,14 @@ class Answer < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :question_id, presence: true
   validates :user_id, presence: true
-  validates :content, presence: true
+  validates :content, presence: true  
+  validate  :image_size
+
+  private
+
+    def image_size
+      if image.size > 5.megabytes
+        errors.add(:picture, "5MB以上は添付できません")
+      end
+    end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -8,8 +8,17 @@ class Question < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, presence: true
+  validate  :image_size
 
   def favorite_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
+  private
+
+    def image_size
+      if image.size > 5.megabytes
+        errors.add(:picture, "5MB以上は添付できません")
+      end
+    end
 end
