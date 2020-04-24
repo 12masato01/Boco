f4c50cfde82b409957bd9c2218093455f380cab2
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-24 19:00:25 +0900

    s3の設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,16 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production?
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAQ4HYINFW4AIVBWPX',
-      aws_secret_access_key: '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',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
     }
 
     config.fog_directory  = 'boco-image'
     config.cache_storage = :fog
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
