cb5dcf2a550e08d8ceb5ffcfbf505b5952e6a237
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-28 18:12:13 +0900

    add:通知を降順に取得

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,5 @@
 class Notification < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
   belongs_to :send_user, class_name: 'User', foreign_key: 'send_user_id'
   belongs_to :notified_user, class_name: 'User', foreign_key: 'notified_user_id'
   belongs_to :notificable, polymorphic: true
