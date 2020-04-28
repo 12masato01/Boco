dd1ce75eda3ec5baefa0768fe37c40334caebdec
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-28 18:08:11 +0900

    add:nitificatuonテーブルとの関連付

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,7 @@
 class Notification < ApplicationRecord
+  belongs_to :send_user, class_name: 'User', foreign_key: 'send_user_id'
+  belongs_to :notified_user, class_name: 'User', foreign_key: 'notified_user_id'
   belongs_to :notificable, polymorphic: true
+  validates :send_user_id, presence: true
+  validates :notified_user_id, presence: true
 end
