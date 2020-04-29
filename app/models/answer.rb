97c4dd0d442f0a8f4dc3c4bd987ae7703c36ab2b
d01masatooo11 <d01masatooo11@gmail.com>
2020-04-29 22:38:54 +0900

    add:通知メソッドの作成

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,6 +9,29 @@ class Answer < ApplicationRecord
   validates :content, presence: true  
   validate  :image_size
 
+  def create_notification_comment!(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(answer_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end 
+
+
+  def save_notification_comment!(current_user, comment_id, notified_user_id)
+    notification = current_user.active_notifications.new(
+      answer_id: id,
+      comment_id: comment_id,
+      notified_user_id: notified_user_id,
+      action: 'comment'
+    )
+
+    if notification.send_user_id == notification.notified_user_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
+
   private
 
     def image_size
--- app/models/question.rb
+++ app/models/question.rb
@@ -15,6 +15,29 @@ class Question < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
+  def create_notification_comment!(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(question_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user, comment_id, temp_id['user_id'])
+    end
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end 
+
+
+  def save_notification_comment!(current_user, comment_id, notified_user_id)
+    notification = current_user.active_notifications.new(
+      question_id: id,
+      comment_id: comment_id,
+      notified_user_id: notified_user_id,
+      action: 'comment'
+    )
+
+    if notification.send_user_id == notification.notified_user_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
+
   private
 
     def image_size
